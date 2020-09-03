484840269d0a3b4a44dcac194b4663f3737ea4cd
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-09-02 19:07:34 -0700

    set state store in app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ToDoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
 
+const todo_items = [{
+  task: "",
+  complete: false,
+  id: ""
+}]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
